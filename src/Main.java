85628764ebd324feeb33b746d1c9ed04d6ed90b6
lvarezhkal <kkakxaa@gmail.com>
2023-12-30 23:45:24 +0300

    allDone

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
+        //1
+        int [] numbers = new int [3];
+        numbers [0] = 1;
+        numbers [1] = 2;
+        numbers [2] = 3;
+        double [] fractions = {1.57, 7.654, 9.986};
+        int [] countdown = {5, 4, 3, 2, 1};
+        //2
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ", ");
+        }  System.out.println();
+        for (int i = 0; i < fractions.length; i++) {
+            System.out.print(fractions[i] + ", ");
+        }  System.out.println();
+        for (int i = 0; i < countdown.length; i++) {
+            System.out.print(countdown[i] + ", ");
+        }  System.out.println();
+        //3
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + ", ");
+        }  System.out.println();
+        for (int i = fractions.length - 1; i >= 0; i--) {
+            System.out.print(fractions[i] + ", ");
+        }  System.out.println();
+        for (int i = countdown.length - 1; i >= 0 ; i--) {
+            System.out.print(countdown[i] + ", ");
+        }  System.out.println();
+        //4
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 != 0){
+                System.out.print(numbers[i] + " ");
+            }else{
+                numbers[i] = numbers[i] + 1;
+                System.out.print(numbers[i] + " ");
+            }
+        }
 
     }
 }
